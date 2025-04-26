a9a7d80714bf8b25b42368dd2a06f5c0fa86b177
Aditya Tile <adityatile99@gmail.com>
2025-04-26 21:17:53 +0530

    setup middleware

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server rinning on port ${PORT}`);
