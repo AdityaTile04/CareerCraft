54e862e1fd41c744e0be7ef3d8d59b8365ff48e5
Aditya Tile <adityatile99@gmail.com>
2025-04-28 22:38:22 +0530

    implement database setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-const PORT = 3000;
+import dotenv from "dotenv";
+import { connectDB } from "./utils/db.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,5 +24,6 @@ app.use(
 );
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server rinning on port ${PORT}`);
 });
