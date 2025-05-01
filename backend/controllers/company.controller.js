6d0f57e2bbd5113d768403b1530c21391feb0c8a
Aditya Tile <adityatile99@gmail.com>
2025-05-01 10:56:52 +0530

    create controller for get company by id

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -49,3 +49,25 @@ export const getCompany = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getCompanyById = async (req, res) => {
+  try {
+    const comapanyId = req.params.id;
+    const company = await Company.findById(comapanyId);
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Company found",
+      success: true,
+      company,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
