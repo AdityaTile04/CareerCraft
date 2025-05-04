c23f9983ad6b6d5d8630479400f42df279e06196
Aditya Tile <adityatile99@gmail.com>
2025-05-04 12:17:04 +0530

    implement category carousel

---
+++ frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Button } from "./ui/button";
+
+const category = [
+  "frontend developer",
+  "backend developer",
+  "fullstack developer",
+  "data analyst",
+  "data engineer",
+  "graphic designer",
+  "data science",
+];
+
+const CategoryCarousel = () => {
+  return (
+    <div className="">
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+              <Button variant="outline" className="rounded-full">
+                {cat}
+              </Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default CategoryCarousel;
