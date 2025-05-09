25fa5a76d6179172392a563c7f2801f1156a8f02
Aditya Tile <adityatile99@gmail.com>
2025-05-09 12:00:00 +0530

    add profile dialog to update user profile

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -12,7 +12,11 @@ const router = express.Router();
 
 router.post("/register", singleUpload, register);
 router.post("/login", login);
+<<<<<<< HEAD
 router.get("/logout", logout);
 router.post("/profile/update", isAuthenticated, singleUpload, updateProfile);
+=======
+router.post("/profile/update", isAuthenticated, updateProfile);
+>>>>>>> parent of 8c6fda4 (add profile dialog to update user profile)
 
 export default router;
