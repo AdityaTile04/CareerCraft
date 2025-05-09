201c6b1c6a6ca0525c1a41d3b4fc79e5ba52a362
Aditya Tile <adityatile99@gmail.com>
2025-05-09 12:23:42 +0530

    create user profile page dynamic

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -7,12 +7,12 @@ import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import AppliedJobTable from "./AppliedJobTable";
 import UpdateProfileDialog from "./UpdateProfileDialog";
-
-const skills = ["html", "css", "javascript", "nodejs", "python", "sql"];
+import { useSelector } from "react-redux";
 
 const isHaveResume = true;
 const Profile = () => {
   const [open, setOpen] = useState(false);
+  const { user } = useSelector((store) => store.auth);
 
   return (
     <div>
@@ -27,11 +27,8 @@ const Profile = () => {
               />
             </Avatar>
             <div>
-              <h1 className="font-medium text-xl">Full Name</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-                sit.
-              </p>
+              <h1 className="font-medium text-xl">{user?.fullname}</h1>
+              <p>{user?.profile?.bio}</p>
             </div>
           </div>
           <Button
@@ -45,20 +42,22 @@ const Profile = () => {
         <div className="my-5">
           <div className="flex items-center gap-3 my-2">
             <Mail />
-            <span>demo@gmail.com</span>
+            <span>{user?.email}</span>
           </div>
 
           <div className="flex items-center gap-3 my-2">
             <Contact />
-            <span>8855559955</span>
+            <span>{user?.phoneNumber}</span>
           </div>
         </div>
 
         <div className="my-5">
           <h1>Skills</h1>
           <div className="flex items-center gap-1">
-            {skills.length !== 0 ? (
-              skills.map((item, index) => <Badge key={index}>{item}</Badge>)
+            {user?.profile?.skills.length !== 0 ? (
+              user?.profile?.skills.map((item, index) => (
+                <Badge key={index}>{item}</Badge>
+              ))
             ) : (
               <span>NA</span>
             )}
