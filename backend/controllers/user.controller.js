f03a5141c7c5ba8ffc5deb5f86c58320f3832bc1
Aditya Tile <adityatile99@gmail.com>
2025-05-13 23:51:36 +0530

    fixed errors

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -127,14 +127,77 @@ export const logout = async (req, res) => {
   }
 };
 
+// export const updateProfile = async (req, res) => {
+//   try {
+//     const { fullname, email, phoneNumber, bio, skills } = req.body;
+//     const file = req.file;
+
+//     //todo: cloudinary upload
+//     const fileUri = getDataUri(file);
+//     const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+
+//     let skillsArray;
+//     if (skills) {
+//       skillsArray = skills.split(",");
+//     }
+
+//     const userId = req.id;
+//     let user = await User.findById(userId);
+
+//     if (!user) {
+//       return res.status(400).json({
+//         message: "User Not Found",
+//         success: false,
+//       });
+//     }
+
+//     if (fullname) user.fullname = fullname;
+//     if (email) user.email = email;
+//     if (phoneNumber) user.phoneNumber = phoneNumber;
+//     if (bio) user.profile.bio = bio;
+//     if (skills) user.profile.skills = skillsArray;
+
+//     //TODO resume comes later
+
+//     if (cloudResponse) {
+//       user.profile.resume = cloudResponse.secure_url;
+//       user.profile.resumeOriginalName = file.originalname;
+//     }
+
+//     await user.save();
+
+//     user = {
+//       _id: user._id,
+//       fullname: user.fullname,
+//       email: user.email,
+//       phoneNumber: user.phoneNumber,
+//       role: user.role,
+//       profile: user.profile,
+//     };
+
+//     return res.status(200).json({
+//       message: "Profile updated successfully",
+//       user,
+//       success: true,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
 export const updateProfile = async (req, res) => {
   try {
     const { fullname, email, phoneNumber, bio, skills } = req.body;
     const file = req.file;
 
-    //todo: cloudinary upload
-    const fileUri = getDataUri(file);
-    const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+    let cloudResponse;
+    let resumeOriginalName;
+
+    if (file) {
+      const fileUri = getDataUri(file);
+      cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+      resumeOriginalName = file.originalname;
+    }
 
     let skillsArray;
     if (skills) {
@@ -157,11 +220,9 @@ export const updateProfile = async (req, res) => {
     if (bio) user.profile.bio = bio;
     if (skills) user.profile.skills = skillsArray;
 
-    //TODO resume comes later
-
     if (cloudResponse) {
       user.profile.resume = cloudResponse.secure_url;
-      user.profile.resumeOriginalName = file.originalname;
+      user.profile.resumeOriginalName = resumeOriginalName;
     }
 
     await user.save();
@@ -182,5 +243,8 @@ export const updateProfile = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
   }
 };
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -12,7 +12,6 @@ import { setUser } from "@/redux/auth.slice";
 
 const Navbar = () => {
   const { user } = useSelector((store) => store.auth);
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -26,35 +25,46 @@ const Navbar = () => {
         navigate("/");
         toast.success(res.data.message);
       }
-    } catch (err) {
-      console.log(err);
-      toast.error(err.response.data.message);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
     }
   };
-
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
-        <div className="">
+        <div>
           <h1 className="text-2xl font-bold">
-            Career<span className="text-[#f83004]">Craft</span>
+            Career<span className="text-[#F83002]">Craft</span>
           </h1>
         </div>
         <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/jobs">Jobs</Link>
-            </li>
-            <li>
-              <Link to="/browse">Browse</Link>
-            </li>
+            {user && user.role === "recruiter" ? (
+              <>
+                <li>
+                  <Link to="/admin/companies">Companies</Link>
+                </li>
+                <li>
+                  <Link to="/admin/jobs">Jobs</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/jobs">Jobs</Link>
+                </li>
+                <li>
+                  <Link to="/browse">Browse</Link>
+                </li>
+              </>
+            )}
           </ul>
-
           {!user ? (
-            <div className="flex gap-2 items-center">
+            <div className="flex items-center gap-2">
               <Link to="/login">
                 <Button variant="outline">Login</Button>
               </Link>
@@ -75,33 +85,38 @@ const Navbar = () => {
                 </Avatar>
               </PopoverTrigger>
               <PopoverContent className="w-80">
-                <div className="flex gap-4 space-y-2">
-                  <Avatar className="cursor-pointer">
-                    <AvatarImage
-                      src={user?.profile?.profilePhoto}
-                      alt="@shadcn"
-                    />
-                  </Avatar>
-                  <div className="">
-                    <h4 className="font-bold">{user?.fullname}</h4>
-                    <p className="text-sm text-muted-foreground">
-                      {user?.profile?.bio}
-                    </p>
+                <div className="">
+                  <div className="flex gap-2 space-y-2">
+                    <Avatar className="cursor-pointer">
+                      <AvatarImage
+                        src={user?.profile?.profilePhoto}
+                        alt="@shadcn"
+                      />
+                    </Avatar>
+                    <div>
+                      <h4 className="font-medium">{user?.fullname}</h4>
+                      <p className="text-sm text-muted-foreground">
+                        {user?.profile?.bio}
+                      </p>
+                    </div>
                   </div>
-                </div>
+                  <div className="flex flex-col my-2 text-gray-600">
+                    {user && user.role === "student" && (
+                      <div className="flex w-fit items-center gap-2 cursor-pointer">
+                        <User2 />
+                        <Button variant="link">
+                          {" "}
+                          <Link to="/profile">View Profile</Link>
+                        </Button>
+                      </div>
+                    )}
 
-                <div className="flex flex-col my-2 text-gray-600">
-                  <div className="flex w-fit items-center gap-2 cursor-pointer">
-                    <User2 />
-                    <Button variant="link">
-                      <Link to="/profile">View Profile</Link>
-                    </Button>
-                  </div>
-                  <div className="flex w-fit items-center gap-2 cursor-pointer">
-                    <LogOut />
-                    <Button onClick={logoutHandler} variant="link">
-                      Logout
-                    </Button>
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <LogOut />
+                      <Button onClick={logoutHandler} variant="link">
+                        Logout
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </PopoverContent>
