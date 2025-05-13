acd47f3a35a5fbff8ca359bc244ee7c5d4125b0e
Aditya Tile <adityatile99@gmail.com>
2025-05-14 00:05:17 +0530

    add job slice with setAllJobs reducer using Redux Toolkit

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth.slice.js";
+import jobSlice from "./job.slice.js";
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
+    job: jobSlice,
   },
 });
 
