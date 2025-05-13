deca931098842d828ded0f121f8bae3ab748fd34
Aditya Tile <adityatile99@gmail.com>
2025-05-14 00:39:35 +0530

    make jobs page dynamic

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -5,9 +5,9 @@ import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { useNavigate } from "react-router-dom";
 
-const Job = () => {
+const Job = ({ job }) => {
   const navigate = useNavigate();
-  const jobId = "abcdefg";
+  // const jobId = "abcdefg";
   return (
     <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
       <div className="flex justify-between items-center">
@@ -23,32 +23,28 @@ const Job = () => {
           </Avatar>
         </Button>
         <div className="">
-          <h1 className="font-medium text-lg">Company Name</h1>
+          <h1 className="font-medium text-lg">{job?.company?.name}</h1>
           <p className="text-sm text-gray-500">India</p>
         </div>
       </div>
       <div>
-        <h1 className="font-bold text-lg my-2">Title</h1>
-        <p className="text-sm text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde iste
-          perspiciatis dolore, ratione qui sapiente beatae esse repellendus
-          facilis enim!
-        </p>
+        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
+        <p className="text-sm text-gray-600">{job?.description}</p>
       </div>
       <div className="flex items-center gap-2 mt-4">
         <Badge className={"text-blue-700 font-bold"} variant="ghost">
-          12 Position
+          {job?.position} Position
         </Badge>
         <Badge className={"text-[#F83002] font-bold"} variant="ghost">
-          Part Time
+          {job?.jobType}
         </Badge>
         <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
-          24 LPA
+          {job?.salary}
         </Badge>
       </div>
       <div className="flex items-center gap-4 mt-4">
         <Button
-          onClick={() => navigate(`/description/${jobId}`)}
+          onClick={() => navigate(`/description/${job._id}`)}
           variant="outline"
         >
           Details
