020679558bb8ec9ad0a516b1919eedaeba5f7230
Aditya Tile <adityatile99@gmail.com>
2025-07-11 22:43:41 +0530

    Add search functionality to search bar

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./shared/Navbar";
 import Job from "./Job";
-
-const randomJobs = [1, 2, 3];
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchedQuery } from "@/redux/job.slice";
+import useGetAllJobs from "@/hooks/useGetAllJobs";
 
 const Browse = () => {
+  useGetAllJobs(); // custom hook that dispatches API call
+  const dispatch = useDispatch();
+
+  // Get actual jobs array from Redux state
+  const { allJobs = [] } = useSelector((store) => store.job);
+
+  useEffect(() => {
+    return () => {
+      dispatch(setSearchedQuery(""));
+    };
+  }, [dispatch]);
+
   return (
     <div>
       <Navbar />
       <div className="max-w-7xl mx-auto my-10">
         <h1 className="font-bold text-xl my-10">
-          Search Results ({randomJobs.length})
+          Search Results ({allJobs.length})
         </h1>
         <div className="grid grid-cols-3 gap-4">
-          {randomJobs.map((item, index) => {
-            return <Job />;
-          })}
+          {allJobs.length === 0 ? (
+            <p>No jobs found.</p>
+          ) : (
+            allJobs.map((job) => <Job key={job._id} job={job} />)
+          )}
         </div>
       </div>
     </div>
--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { Search } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { setSearchedQuery } from "@/redux/job.slice";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+  const { query, setQuery } = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const searchJobHandler = () => {
+    dispatch(setSearchedQuery(query));
+    navigate("/browse");
+  };
+
   return (
     <div className="text-center">
       <div className="flex flex-col gap-5 my-10">
@@ -21,9 +33,13 @@ const HeroSection = () => {
           <input
             type="text"
             placeholder="Find your dream job"
+            onChange={(e) => setQuery(e.target.value)}
             className="outline-none border-none w-full"
           />
-          <Button className="rounded-r-full bg-[#6A38C2] text-white hover:bg-[#532b97]  ">
+          <Button
+            onClick={searchJobHandler}
+            className="rounded-r-full bg-[#6A38C2] text-white hover:bg-[#532b97]  "
+          >
             <Search className="h-5 w-5" />
           </Button>
         </div>
