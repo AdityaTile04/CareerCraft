01800722ea1f665f170171017ec532eb1f9c039f
Aditya Tile <adityatile99@gmail.com>
2025-07-12 12:45:55 +0530

    add backend url

--- frontend/src/constants/data.js
+++ frontend/src/constants/data.js
@@ -5,10 +5,10 @@
 // export const COMPANY_API_END_POINT = "http://localhost:3000/api/company";
 
 export const USER_API_END_POINT =
-  "https://careercraft-jfpu.onrender.com/api/user";
+  "https://careercraft-backend-dnxl.onrender.com/api/user";
 export const JOB_API_END_POINT =
-  "https://careercraft-jfpu.onrender.com/api/job";
+  "https://careercraft-backend-dnxl.onrender.com/api/job";
 export const APPLICATION_API_END_POINT =
-  "https://careercraft-jfpu.onrender.com/api/application";
+  "https://careercraft-backend-dnxl.onrender.com/api/application";
 export const COMPANY_API_END_POINT =
-  "https://careercraft-jfpu.onrender.com/api/company";
+  "https://careercraft-backend-dnxl.onrender.com/api/company";
