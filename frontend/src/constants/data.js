6396cb7e5f919321dd4280ddcf9a9bf71e571476
Aditya Tile <adityatile99@gmail.com>
2025-07-12 12:08:58 +0530

    add deployed url

--- frontend/src/constants/data.js
+++ frontend/src/constants/data.js
@@ -1,5 +1,14 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/user";
-export const JOB_API_END_POINT = "http://localhost:3000/api/job";
+// export const USER_API_END_POINT = "http://localhost:3000/api/user";
+// export const JOB_API_END_POINT = "http://localhost:3000/api/job";
+// export const APPLICATION_API_END_POINT =
+//   "http://localhost:3000/api/application";
+// export const COMPANY_API_END_POINT = "http://localhost:3000/api/company";
+
+export const USER_API_END_POINT =
+  "https://careercraft-jfpu.onrender.com/api/user";
+export const JOB_API_END_POINT =
+  "https://careercraft-jfpu.onrender.com/api/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:3000/api/application";
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/company";
+  "https://careercraft-jfpu.onrender.com/api/application";
+export const COMPANY_API_END_POINT =
+  "https://careercraft-jfpu.onrender.com/api/company";
